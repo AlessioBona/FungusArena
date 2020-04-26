993ecefc06d0bf31c674e9d7c4fca40cc3e5e2a0
AlessioBona <alefbona@gmail.com>
2020-04-26 14:16:16 +0200

    Initial commit

---
+++ Assets/GameOfLife.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GameOfLife
+{
+    
+        //int[,] statusArray = new int[5, 5] { { 1,0,1,0,0}, { 1, 0, 1, 0, 0 } , { 1, 0, 1, 0, 0 } , { 1, 0, 1, 0, 0 } , { 1, 0, 1, 0, 0 } };
+        //int[,] toRenderArray = new int[5, 5] { { 1, 0, 1, 0, 0 }, { 1, 0, 1, 0, 0 }, { 1, 0, 1, 0, 0 }, { 1, 0, 1, 0, 0 }, { 1, 0, 1, 0, 0 } };
+        
+    
+         
+    public static void UpdateCellStatus(int[,] status, int[,] toRender)
+    {
+        int arraySizeX = status.GetLength(0);
+        int arraySizeY = status.GetLength(1);
+        //Debug.Log(arraySizeX);
+        //Debug.Log(arraySizeY);
+        //loop through status array and check neighbors of each cell
+        for(int j = 0; j < arraySizeX; j++) //columns
+        {
+            for(int i = 0; i < arraySizeY; i++) // rows
+            {
+                int sumOfNeighbors = 0;
+                //calculate number of alive neighbors
+                for(int x = Math.Max(j-1, 0); x < Math.Min(j+2, arraySizeX); x++) //columns of neighbors
+                {
+                    for(int y = Math.Max(i-1, 0); y < Math.Min(i+2, arraySizeY); y++){
+                        sumOfNeighbors += status[y, x];
+                    }
+                }
+
+                //find out the new status of the respective cell:
+                int cellStatus = status[i, j];
+                sumOfNeighbors -= cellStatus; //subtract cell status because it was part of the sum of neighbors
+
+                toRender[i, j] = cellStatus;
+
+                switch (cellStatus)
+                {
+                    case 1:
+                        switch (sumOfNeighbors)
+                        {
+                            case 2:
+                                break;
+                            case 3:
+                                break;
+                            default:
+                                toRender[i, j] = 0;
+                                break;
+                        }
+                        break;
+                    case 0:
+                        switch (sumOfNeighbors)
+                        {
+                            case 3:
+                                toRender[i, j] = 1;
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                    default:
+                        break;
+
+                }
+            }
+        }
+        //PrintArray(toRender);
+    }
+
+    private static void PrintArray(int[,] arr)
+    {
+        int rowLength = arr.GetLength(0);
+        int colLength = arr.GetLength(1);
+
+        for (int i = 0; i < rowLength; i++)
+        {
+            for (int j = 0; j < colLength; j++)
+            {
+                Console.Write(string.Format("{0} ", arr[i, j]));
+            }
+            Console.Write(Environment.NewLine + Environment.NewLine);
+        }
+        Console.ReadLine();
+    }
+}
---
+++ Assets/Vitrus_Genenator_2.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vitrus_Genenator_2 : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    Texture2D texture;
+    Sprite mySprite;
+    int textureWidth = 256;
+    int textureHeight = 256;
+
+    public Color[] colors;
+    //public Color[] colors2;
+    //int actualColor = 0;
+
+    [Header("Game of Live vars")]
+    public int matrixSize = 32;
+    public int cellSize = 4;
+
+    int[,] matrix_1;
+    int[,] matrix_2;
+    int[,] toRender;
+
+    // definiert welche matrix zur Updatefunktion als Referenz und welche als Output weitergegeben wird
+    bool actualMatrix = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PrepareRenderer();
+        Restart();
+        //matrix_1 = new int[,] { {1 , 0 }, {0 , 1 } };
+        //matrix_2 = matrix_1;
+        //ChangeArray(matrix_2);
+        //Debug.Log(matrix_1[0, 0]);
+
+
+    }
+
+    public void Restart()
+    {
+        PrepareMatrixes();
+        timer = refreshTime;
+        actualMatrix = true;
+    }
+
+    //private void ChangeArray(int[,] toChange)
+    //{
+    //    toChange[0, 0] = 0;
+    //}
+
+    private void PrepareMatrixes()
+    {
+        // prepare matrix_1
+        matrix_1 = new int[matrixSize, matrixSize];
+        matrix_2 = new int[matrixSize, matrixSize];
+
+        for (int x = 0; x < matrixSize; x++)
+        {
+            for (int y = 0; y < matrixSize; y++)
+            {
+                matrix_1[x, y] = UnityEngine.Random.Range(0, 2);
+            }
+        }
+
+        toRender = matrix_1;
+        SetRenderOutput();
+    }
+
+
+
+    private void PrepareRenderer()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        textureWidth = matrixSize * cellSize;
+        textureHeight = matrixSize * cellSize;
+        texture = new Texture2D(textureWidth, textureHeight);
+        Rect myRect = new Rect(0, 0, textureWidth, textureHeight);
+        mySprite = Sprite.Create(texture, myRect, new Vector2(.5f, .5f));
+        spriteRenderer.sprite = mySprite;
+    }
+
+    public float refreshTime = 1f;
+    float timer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // mit timer
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            SetRenderOutput();
+            PlayRound();
+            timer = refreshTime;
+        }
+    }
+
+    private void PlayRound()
+    {
+        if (actualMatrix)
+        {
+            GameOfLife.UpdateCellStatus(matrix_1, matrix_2);
+            // funzione con matrix 1
+            toRender = matrix_2;
+        } else
+        {
+            GameOfLife.UpdateCellStatus(matrix_2, matrix_1);
+            // funzione con matrix 2
+            toRender = matrix_1;
+        }
+
+        actualMatrix = !actualMatrix;
+    }
+
+    private void FakeGame(int[,] intro, int[,] outro)
+    {
+
+    }
+
+    private void ColorCell(int ox, int oy, Color color)
+    {
+        for (int x = ox * cellSize; x < ox * cellSize + cellSize; x++)
+        {
+            for (int y = oy * cellSize; y < oy * cellSize + cellSize; y++)
+            {
+                texture.SetPixel(x, y, color);
+            }
+        }
+    }
+
+    private void SetRenderOutput()
+    {
+        // cell Size e toRender
+
+        for(int x = 0; x < matrixSize; x++)
+        {
+            for (int y = 0; y < matrixSize; y++)
+            {
+
+                if (toRender[x, y] == 1)
+                {
+                    ColorCell(x, y, colors[1]);
+                }
+                else
+                {
+                    ColorCell(x, y, colors[0]);
+                }
+            }
+
+        }
+
+
+        //for (int y = 1; y < texture.height; y = y + 2)
+        //{
+        //    for (int x = 1; x < texture.width; x = x + 2)
+        //    {
+        //        Color color = colors[actualColor];
+        //        texture.SetPixel(x, y, color);
+        //    }
+        //}
+
+        //for (int y = 0; y < texture.height; y = y + 2)
+        //{
+        //    for (int x = 0; x < texture.width; x = x + 2)
+        //    {
+        //        Color color = colors2[actualColor];
+        //        texture.SetPixel(x, y, color);
+        //    }
+        //}
+
+        texture.Apply();
+    }
+}
